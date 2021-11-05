3698deced3e382d0f26e2196ad99ea5eccd625f9
liangzw7 <1901543053@qq.com>
2021-11-05 09:00:25 +0800

    添加cleanWebpackPlugin

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,17 +1,18 @@
-
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, '..', 'src/main.js'),
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './template/index.html'
-        })
-    ]
+  mode: 'development',
+  entry: path.resolve(__dirname, '..', 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 }
